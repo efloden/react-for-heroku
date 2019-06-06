6ac07f575b9633340c5c132761887e931a89b813
earl.floden <earl.floden@rakuten.com>
2019-06-06 17:05:32 +0900

    read env var in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,49 @@
 var webpack = require('webpack');
 
-module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-	  new webpack.DefinePlugin({
-		  __API__: process.env.API_CONST
-	  })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true
-            }
-          }
-        ]
-      }
-    ]
-  }
+module.exports = env => {
+	console.log('API CONST WAS: ', env.API_CONST)
+	return {
+		entry: [
+			'./src/index.js'
+		  ],
+		  output: {
+			path: __dirname,
+			publicPath: '/',
+			filename: 'bundle.js'
+		  },
+		  plugins: [
+			  new webpack.DefinePlugin({
+				  __API__: '"'+env.API_CONST+'"'
+			  })
+		  ],
+		  module: {
+			rules: [
+			  {
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+				  loader: "babel-loader"
+				}
+			  },
+			  {
+				test: /\.css$/,
+				use: [
+				  {
+					loader: "style-loader"
+				  },
+				  {
+					loader: "css-loader",
+					options: {
+					  modules: true,
+					  importLoaders: 1,
+					  localIdentName: "[name]_[local]_[hash:base64]",
+					  sourceMap: true,
+					  minimize: true
+					}
+				  }
+				]
+			  }
+			]
+		  }
+	}
 };
